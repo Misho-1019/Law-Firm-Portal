be4a3e0737f58116b7fd7ee6a36cd31f16a0f662
Misho1019 <mikailtorres99@gmail.com>
2025-09-11 00:09:18 +0300

    Clear user session for invalid auth tokens

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -14,6 +14,8 @@ export const authMiddleware = (req, res, next) => {
 
         next();
     } catch (error) {
-        //TODO:
+        res.clearCookie('auth');
+
+        return res.status(401).json({ message: 'Invalid or expired token' })
     }
 }
