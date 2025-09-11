0323f3a22cc198dedb3e63ca2f6da74047a4d2f4
Misho1019 <mikailtorres99@gmail.com>
2025-09-11 23:43:37 +0300

    Add validation to the User model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -5,16 +5,24 @@ const userSchema = new Schema({
     username: { 
         type: String, 
         required: true, 
-        unique: true 
+        minLength: 2,
+        unique: true,
+        trim: true, 
     },
     email: { 
         type: String, 
         required: true, 
-        unique: true 
+        unique: true,
+        lowercase: true,
+        match: /\@[a-zA-Z]+.[a-zA-Z]+$/,
+        minLength: 8, 
     },
     password: { 
         type: String, 
-        required: true 
+        required: true,
+        minLength: 6,
+        match: /^\w+$/,
+        trim: true,
     }
 })
 
