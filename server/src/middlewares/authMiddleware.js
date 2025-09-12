21cd0bb216123221deb178781a9ae8e9276f2f78
Misho1019 <mikailtorres99@gmail.com>
2025-09-12 23:56:27 +0300

    Add route guards for login and register

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -27,3 +27,11 @@ export const isAuth = (req, res, next) => {
 
     next();
 }
+
+export const isGuest = (req, res, next) => {
+    if(req.user) {
+        return res.status(403).json({ message: 'You are already logged in'  })
+    }
+
+    next();
+}
