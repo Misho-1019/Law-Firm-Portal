ba53fa7cb310692a62d75294cd7f33beb3b62d65
Misho1019 <mikailtorres99@gmail.com>
2025-09-14 11:20:06 +0300

    Add appointment controller in routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authController from "./controllers/authController.js";
+import appointmentController from "./controllers/appointmentController.js";
 
 const router = Router();
 
 router.use('/auth', authController);
+router.use('/appointments', appointmentController)
 
 export default router;
