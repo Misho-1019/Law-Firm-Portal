bd9c22e1a7367b9b8711c66dd67e976ea3581e0a
Misho1019 <mikailtorres99@gmail.com>
2025-09-18 06:52:30 +0300

    Install and set up cors

--- server/src/index.js
+++ server/src/index.js
@@ -5,10 +5,16 @@ dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import router from "./routes.js";
 import { authMiddleware } from "./middlewares/authMiddleware.js";
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}))
 
 try {
     const uri = process.env.MONGO_URI;
