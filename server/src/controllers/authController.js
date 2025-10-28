d1efc6fcb9902a889827f932d8fbd26d2820ddd7
Misho1019 <mikailtorres99@gmail.com>
2025-10-28 06:56:58 +0200

    Auth cookie flags

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -4,13 +4,15 @@ import { isAuth, isGuest } from "../middlewares/authMiddleware.js";
 
 const authController = Router();
 
+const isProd = process.env.NODE_ENV === 'production';
+
 authController.post('/register', isGuest, async (req, res) => {
     const authData = req.body;
 
     try {
         const token = await authService.register(authData);
 
-        res.cookie('auth', token, { httpOnly: true })
+        res.cookie('auth', token, { httpOnly: true, secure: isProd, sameSite: isProd ? 'lax' : 'lax', maxAge: 2 * 60 * 60 * 1000 }); // 2 hours
 
         res.status(201).json({ message: 'User registered successfully' })
     } catch (error) {
@@ -24,7 +26,7 @@ authController.post('/login', isGuest, async (req, res) => {
     try {
         const token = await authService.login(email, password);
 
-        res.cookie('auth', token, { httpOnly: true }); 
+        res.cookie('auth', token, { httpOnly: true, secure: isProd, sameSite: isProd ? 'lax' : 'lax', maxAge: 2 * 60 * 60 * 1000 }); // 2 hours
 
         res.status(200).json({ message: 'Logged in successfully' });
     } catch (err) {
